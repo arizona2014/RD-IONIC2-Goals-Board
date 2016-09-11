ace7ff4034d6d3ae4371ccee06af447b975c1801
arizona2014 <andy.lisac@gmail.com>
2016-09-11 20:44:53 +0200

    Adding some functionality

--- js/controllers.js
+++ js/controllers.js
@@ -1,13 +1,24 @@
 angular.module('app.controllers', [])
-  
+
 .controller('goalsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
 function ($scope, $stateParams) {
 
+  $scope.addButton = function(){
+    console.log("Add button clicked");
+  };
 
+  $scope.editButton = function(){
+    console.log("Edit button clicked");
+  };
+
+  $scope.deleteButton = function(){
+    console.log("Delete button clicked");
+  };
+  
 }])
-   
+
 .controller('visualizeCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -15,7 +26,7 @@ function ($scope, $stateParams) {
 
 
 }])
-   
+
 .controller('statsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
@@ -23,4 +34,3 @@ function ($scope, $stateParams) {
 
 
 }])
-    
