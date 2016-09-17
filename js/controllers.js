4b955c2a6ac5ed39c153c2f4686bc867e4beb996
arizona2014 <andy.lisac@gmail.com>
2016-09-17 22:31:24 +0200

    Included PouchService into controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,11 +1,12 @@
 angular.module('app.controllers', [])
 
-.controller('goalsCtrl', ['$scope', '$stateParams', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('goalsCtrl', ['$scope', '$stateParams', 'PouchService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams) {
+function ($scope, $stateParams, PouchService) {
 
-  var db = new PouchDB('my_database');
+  var db = PouchService.db;
+  console.log(db);
 
   $scope.addButton = function(){
     console.log("Add button clicked");
