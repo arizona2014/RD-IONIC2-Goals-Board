bd878e8cb0598fee9666e18eaff3213d9a43dfd5
arizona2014 <andy.lisac@gmail.com>
2016-09-18 15:27:58 +0200

    basic CRUD functionality with Pouch DB in controller

--- js/controllers.js
+++ js/controllers.js
@@ -1,12 +1,20 @@
 angular.module('app.controllers', [])
 
-.controller('goalsCtrl', ['$scope', '$stateParams', 'PouchService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
+.controller('goalsCtrl', ['$scope', '$rootScope', '$stateParams', 'PouchService', // The following is the constructor function for this page's controller. See https://docs.angularjs.org/guide/controller
 // You can include any angular dependencies as parameters for this function
 // TIP: Access Route Parameters for your page via $stateParams.parameterName
-function ($scope, $stateParams, PouchService) {
+function ($scope, $rootScope, $stateParams, PouchService) {
 
   var db = PouchService.db;
-  console.log(db);
+
+  db.allDocs({
+    include_docs: true
+  }).then(function(result) {
+    $rootScope.goals = result.rows;
+    console.log($rootScope.goals);
+  }).catch(function(err) {
+    console.log(err);
+  });
 
   $scope.addButton = function(){
     console.log("Add button clicked");
